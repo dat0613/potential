517f559df1ea692bbecda73257ecd30c79365e9e
dat0613 <dat0613@naver.com>
2022-03-03 23:03:40 +0900

    문자열 인코딩 테스트

--- Source/Serialization/Serialization.cpp
+++ Source/Serialization/Serialization.cpp
@@ -1,12 +1,21 @@
 ﻿#include <iostream>
 #include "flatbuffers/SchemaTest_generated.h"
+#include <unicode/ustring.h>
+#include <unicode/unistr.h>
+#include <utility>
+#include <Windows.h>
 
 int main()
 {
+	SetConsoleOutputCP(CP_UTF8);
+	setlocale(LC_ALL, "en_US.UTF-8");
+
 	flatbuffers::FlatBufferBuilder builder;
 	MyGame::Vec3 vec3{ 123.456f, 789.123f, 456.789f };
 	std::vector<unsigned char> vector{ 12, 34, 56, 78, 90 };
-	auto monster = MyGame::CreateMonster(builder, &vec3, 12345, 9876, builder.CreateString("Monster"), builder.CreateVector(vector), MyGame::Color_Green);
+
+	std::string name = "اللغة العربية 한국어 日本語 妈妈, 我们不能养一只波斯猫吗 คุณด้วยนะคะ Монгол хэл русский français";
+	auto monster = MyGame::CreateMonster(builder, &vec3, 12345, 9876, builder.CreateString(name), builder.CreateVector(vector), MyGame::Color_Green);
 
 	builder.Finish(monster);
 
@@ -27,4 +36,30 @@ int main()
 	std::cout << std::endl;
 
 	std::cout << "color : " << (int)monster2->color() << std::endl;
+
+
+
+	std::u8string u8string = u8"اللغة العربية 한국어 日本語 妈妈, 我们不能养一只波斯猫吗 คุณด้วยนะคะ Монгол хэл русский français";
+	std::string string = reinterpret_cast<const char*>(u8"اللغة العربية 한국어 日本語 妈妈, 我们不能养一只波斯猫吗 คุณด้วยนะคะ Монгол хэл русский français");
+
+	std::cout << sizeof(char8_t) << std::endl;;
+	std::cout << sizeof(char) << std::endl;;
+
+
+	auto u8stringBuffer = u8string.c_str();
+	auto stringBuffer = string.c_str();
+	std::cout << reinterpret_cast<const char*>(u8string.c_str()) << std::endl;
+	for (int i = 0; i < u8string.length(); i++)
+	{
+		std::cout << static_cast<int>(u8stringBuffer[i]) << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "-------------------------------------------------------------------------------" << std::endl;
+	std::cout << std::endl;
+
+	for (int i = 0; i < string.length(); i++)
+	{
+		std::cout << static_cast<int>(static_cast<unsigned char>(stringBuffer[i])) << std::endl;
+	}
 }
